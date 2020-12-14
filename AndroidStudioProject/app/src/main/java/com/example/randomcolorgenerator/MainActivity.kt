092c424b9659b9c580dbfd9a6cb78f979ac3d043
SuperSpicyCatfish <julia4realz@gmail.com>
2020-12-13 20:46:55 -0800

    Button/string/etc

--- AndroidStudioProject/app/src/main/java/com/example/randomcolorgenerator/MainActivity.kt
+++ AndroidStudioProject/app/src/main/java/com/example/randomcolorgenerator/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.randomcolorgenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var genColorButton: Button
+
+    private lateinit var textView1: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        genColorButton = findViewById(R.id.gen_color_button)
+        textView1 = findViewById(R.id.textView1)
+
+
+        genColorButton.setOnClickListener { view: View ->
+            textView1.setBackgroundColor(resources.getColor(R.color.teal_200))
+            textView1.setText("boop")
+
+        }
+
+
     }
 }
