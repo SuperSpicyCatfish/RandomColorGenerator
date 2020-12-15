bf5861bc9c0e86f49b1ec39a0f1effb9a3e459df
SuperSpicyCatfish <julia4realz@gmail.com>
2020-12-14 21:46:59 -0800

    Changing Button to Spinner
    - added a String array for the spinner
    - adds ids for all textViews
    - moved all strings to strings.xml, no longer hard coded
    - created a fourth color textView
    - added a Color Picker textView, simply to separate from the generated colors and the color picker

--- AndroidStudioProject/app/src/main/java/com/example/randomcolorgenerator/MainActivity.kt
+++ AndroidStudioProject/app/src/main/java/com/example/randomcolorgenerator/MainActivity.kt
@@ -4,14 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Spinner
 import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var genColorButton: Button
-
     private lateinit var textView1: TextView
+    private lateinit var spinner1: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
         genColorButton = findViewById(R.id.gen_color_button)
         textView1 = findViewById(R.id.textView1)
+        spinner1 = findViewById(R.id.spinner1)
 
 
         genColorButton.setOnClickListener { view: View ->
