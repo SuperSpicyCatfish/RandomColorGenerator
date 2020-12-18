4528dd6fe3fb97ada7740bd039e5fc6d216c6f5c
Dungtran713 <trandung713@gmail.com>
2020-12-17 17:52:40 -0800

    Update MainActivity.kt

--- AndroidStudioProject/app/src/main/java/com/example/randomcolorgenerator/MainActivity.kt
+++ AndroidStudioProject/app/src/main/java/com/example/randomcolorgenerator/MainActivity.kt
@@ -17,7 +17,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var genColorButton: Button
     private lateinit var textView1: TextView
     private lateinit var spinner1: Spinner
-
+    private lateinit var bitmap: Bitmap
+    private lateinit var colorwheel: ImageView
+    private lateinit var colorbar: View
+    private lateinit var resultcolor: TextView
     var selectedColor = 0 // the random created color
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -28,6 +31,9 @@ class MainActivity : AppCompatActivity() {
         genColorButton = findViewById(R.id.gen_color_button)
         textView1 = findViewById(R.id.textView1)
         spinner1 = findViewById(R.id.spinner1)
+          colorwheel = findViewById(R.id.colorwheel)
+        colorbar = findViewById(R.id.colorbar)
+        resultcolor = findViewById(R.id.resultcolor)
 
 
         // this is for the spinner
@@ -52,7 +58,25 @@ class MainActivity : AppCompatActivity() {
 
 
         }
+            colorwheel.isDrawingCacheEnabled = true
+            colorwheel.buildDrawingCache(true)
 
+
+            colorwheel.isDrawingCacheEnabled = true
+            colorwheel.buildDrawingCache(true)
+            colorwheel.setOnTouchListener { v, event ->
+                 if (event.action == MotionEvent.ACTION_DOWN || event.action == MotionEvent.ACTION_MOVE) {
+                    bitmap = colorwheel.getDrawingCache()
+                    val pixel = bitmap.getPixel(event.x.toInt(), event.y.toInt())
+                    val r = Color.red(pixel)
+                    val g = Color.green(pixel)
+                    val b = Color.blue(pixel)
+                    val hex = "#" + Integer.toHexString(pixel)
+                    colorbar.setBackgroundColor(Color.rgb(r, g, b))
+                    resultcolor.text = "RGB: $r, $g, $b \nHEX: $hex"
+                    }
+                 true
+        }
     }
 
     fun  allColors(){ // the range of this is all
