a8f164db710e3419bb6869cfba44759ffbba47ea
SuperSpicyCatfish <julia4realz@gmail.com>
2020-12-19 19:26:44 -0800

    Generating Color options
    - All Colors option completed
    - Monochromatic option completed

--- AndroidStudioProject/app/src/main/java/com/example/randomcolorgenerator/MainActivity.kt
+++ AndroidStudioProject/app/src/main/java/com/example/randomcolorgenerator/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.randomcolorgenerator
 
 import android.app.Activity
+import android.graphics.Bitmap
 import android.graphics.Color
 import android.graphics.Color.*
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
 import android.widget.*
 import androidx.annotation.RequiresApi
+import androidx.core.graphics.blue
+import androidx.core.graphics.green
+import androidx.core.graphics.red
 import kotlin.random.Random
 
 
@@ -16,6 +21,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var genColorButton: Button
     private lateinit var textView1: TextView
+    private lateinit var textView2: TextView
+    private lateinit var textView3: TextView
+    private lateinit var textView4: TextView
     private lateinit var spinner1: Spinner
     private lateinit var bitmap: Bitmap
     private lateinit var colorwheel: ImageView
@@ -30,8 +38,11 @@ class MainActivity : AppCompatActivity() {
 
         genColorButton = findViewById(R.id.gen_color_button)
         textView1 = findViewById(R.id.textView1)
+        textView2 = findViewById(R.id.textView2)
+        textView3 = findViewById(R.id.textView3)
+        textView4 = findViewById(R.id.textView4)
         spinner1 = findViewById(R.id.spinner1)
-          colorwheel = findViewById(R.id.colorwheel)
+        colorwheel = findViewById(R.id.colorwheel)
         colorbar = findViewById(R.id.colorbar)
         resultcolor = findViewById(R.id.resultcolor)
 
@@ -51,9 +62,43 @@ class MainActivity : AppCompatActivity() {
                 allColors() // generates the new color
                 textView1.setBackgroundColor(selectedColor)
                 textView1.setText("#"+Integer.toHexString(selectedColor))
+
+                allColors()
+                textView2.setBackgroundColor(selectedColor)
+                textView2.setText("#"+Integer.toHexString(selectedColor))
+
+                allColors()
+                textView3.setBackgroundColor(selectedColor)
+                textView3.setText("#"+Integer.toHexString(selectedColor))
+
+                allColors()
+                textView4.setBackgroundColor(selectedColor)
+                textView4.setText("#"+Integer.toHexString(selectedColor))
+
+            }
+            else if(spinner1.selectedItemPosition == 1) {
+                monochromatic(0)
+                textView1.setBackgroundColor(selectedColor)
+                textView1.setText("#"+Integer.toHexString(selectedColor))
+
+                monochromatic(1)
+                textView2.setBackgroundColor(selectedColor)
+                textView2.setText("#"+Integer.toHexString(selectedColor))
+
+                monochromatic(1)
+                textView3.setBackgroundColor(selectedColor)
+                textView3.setText("#"+Integer.toHexString(selectedColor))
+
+                monochromatic(1)
+                textView4.setBackgroundColor(selectedColor)
+                textView4.setText("#"+Integer.toHexString(selectedColor))
+
             }
             else{ // FOR TESTING PURPOSES, ANY OTHER OPTION SELECTED
                 textView1.setText("else")
+                textView2.setText("else")
+                textView3.setText("else")
+                textView4.setText("else")
             }
 
 
@@ -84,6 +129,14 @@ class MainActivity : AppCompatActivity() {
         selectedColor = Color.argb((Random.nextInt(0,255)), Random.nextInt(0,255), Random.nextInt(0,255), Random.nextInt(0,255))
     }
 
+    fun monochromatic(givenColor : Int){
+        if(givenColor == 0){
+            selectedColor = Color.argb((Random.nextInt(0,255)), Random.nextInt(0,255), Random.nextInt(0,255), Random.nextInt(0,255))
+        }
+        else{
+            selectedColor = Color.argb((Random.nextInt(0,255)), selectedColor.red, selectedColor.green, selectedColor.blue)
+        }
+    }
 
 
 
